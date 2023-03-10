c16cd6c6dc05140ad6233555e8317548fc067731
Aninha <aninhapss2000@gmail.com>
2023-03-09 23:12:43 -0300

    Added controller two, label, image, button

---
+++ ThreeProjectGitHub/ThreeProjectGitHub/TwoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TwoViewController.swift
+//  ThreeProjectGitHub
+//
+//  Created by Ana Paula Silva de Sousa on 09/03/23.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet var button: UIButton!
+    @IBOutlet var imageTwo: UIImageView!
+    @IBOutlet var labelOne: UILabel!
+    @IBOutlet var labelTwo: UILabel!
+    
+    var serieTwo: BrazilianSeries = BrazilianSeries(name: "", descriptionSerie: "", chronology: "", characters: "", seasons: "", image: "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func back(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func follow(_ sender: Any) {
+    }
+}
